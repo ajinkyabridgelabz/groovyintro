3e571e787375d4a155bca5db95aec9fd92255436
satya <sspra143@gmail.com>
2023-09-21 15:25:44 +0530

    Added:Escaping specia chracter in groovy

--- main.groovy
+++ main.groovy
@@ -1,1 +1,10 @@
 // Every one write down the code here
+def myString = "This is a \\\"quoted\\\" string";
+println myString
+def myString2 = "This is a backslash: \\\\";
+println myString2
+def myString5 = "Line 1\\nLine 2";
+println myString5
+def myString6 = "Before\\tAfter";
+println myString6
+
