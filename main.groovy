dd9b76524f997c9d282884c2fdaedfcb8cc6a4ca
naveenkumargorakala <nicenaveengorakala@gmail.com>
2023-09-21 16:03:14 +0530

    Add:GroovyDoc with MultiLine Comment

--- main.groovy
+++ main.groovy
@@ -1,29 +1,26 @@
 import org.codehaus.groovy.runtime.StringBufferWriter
 
-
 // Every one write down the code here
 println("Hello World")
 
 
    println "/* */ are used for multiline comments";
-   /*
-   *In the max method ,
-   * we are given three numbers
-   * and find the largest among them
-   * by using Ternary operator
-   * First we are checking whether "a" is greater than "b" and "c"
-   * if it is true it returns "a" or else
-   * check "b" greater than "c" .
-   * if it is true print "b" or else
-   * print "c"
-   */
-   public static void max() {
-      def a=32;
-      def b=45;
-      def c=11;
-      println(" $a ,$b, $c maximum number is :"+((a > b && a > c) ? a : (b > c) ? b : c));
-   }
-//method call
-max()
 
 
+  /*
+    *In the main method ,
+    * we are given three numbers
+    * and find the largest among them
+    * by using Ternary operator
+    * First we are checking whether "a" is greater than "b" and "c"
+    * if it is true it returns "a" or else
+    * check "b" greater than "c" .
+    * if it is true print "b" or else
+    * print "c"
+     */
+    static void main(String[] args) {
+        def a=32;
+        def b=45;
+        def c=11;
+        println(" $a ,$b, $c maximum number is :"+((a > b && a > c) ? a : (b > c) ? b : c));
+    }
