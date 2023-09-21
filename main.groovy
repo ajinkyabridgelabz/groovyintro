c80c485eb47b10e3e6d67d6d8bf78534b112cf0b
pranali <pranalikubade2000@gmail.com>
2023-09-21 14:58:51 +0530

    [Pranali: Add Single-line comment in groovy Added]

--- main.groovy
+++ main.groovy
@@ -1,1 +1,5 @@
 // Every one write down the code here
+
+println "Single-line comments start with // and can be found at any position in the line. The characters following //, until the end of the line, are considered part of the comment."
+// a standalone single line comment
+println "hello" // a comment till the end of the line
