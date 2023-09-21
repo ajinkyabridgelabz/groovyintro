2e3ffecd192b439f176581502031c6a893f349d6
satya <sspra143@gmail.com>
2023-09-21 17:01:10 +0530

    Added:in groovy multi catch block

--- main.groovy
+++ main.groovy
@@ -1,33 +1,12 @@
-// logical &&
-def a = true
-def b = false
-def result = a && b  // result is false
-println result;
+// multi catch block
+try {
+    // Code that may throw exceptions
+    def result = 10 / 0
+} catch (ArithmeticException e) {
+    println "Arithmetic Exception: ${e.message}"
+} catch (NullPointerException e) {
+    println "Null Pointer Exception: ${e.message}"
+} catch (Exception e) {
+    println "General Exception: ${e.message}"
+}
 
-// logical or ||
-def a2 = true
-def b2 = false
-def res = a || b
-println res;
-
-// logical not !
-def f = true
-def result3 = !f  // result is false
-println result3;
-
-//logical exclusive Or (XOR)
-def m = true
-def n = false
-def result5 = (m && !n) || (!m && n)
-println result5;
-
-//ternairy operators
-def k = true
-def result6 = (k) ? "It's true" : "It's false"
-println result6
-
-//Null Coalescing Operator (?:):
-def p = null
-def q = "Hello"
-def result7 = p ?: q  // result is "Hello"
-println result7
