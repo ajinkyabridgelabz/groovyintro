1333f479713823a2eb4cec81da2838b176ec90c5
satya <sspra143@gmail.com>
2023-09-21 16:23:59 +0530

    Added: groovy keywords with exmple

--- main.groovy
+++ main.groovy
@@ -1,10 +1,57 @@
-// Every one write down the code here
-def myString = "This is a \\\"quoted\\\" string";
-println myString
-def myString2 = "This is a backslash: \\\\";
-println myString2
-def myString5 = "Line 1\\nLine 2";
-println myString5
-def myString6 = "Before\\tAfter";
-println myString6
+// static keyword
+class MyClass {
+    static int staticVariable = 10
 
+    static void staticMethod() {
+        println("This is a static method")
+    }
+}
+
+println(MyClass.staticVariable)
+MyClass.staticMethod()
+
+// super keyword
+
+class ChildClass extends ParentClass {
+    void methodInChildClass() {
+        super.methodInParentClass()
+    }
+}
+//switch keyword
+
+def fruit = 'apple'
+
+switch (fruit) {
+    case 'apple':
+        println('Selected fruit is apple')
+        break
+    case 'banana':
+        println('Selected fruit is banana')
+        break
+    default:
+        println('Unknown fruit')
+}
+
+//synchronized keyword
+synchronized (someObject) {
+    // Code that should be synchronized
+}
+
+// this keyword
+class Satya {
+    def name
+
+    MyClass(String name) {
+        this.name = name
+    }
+}
+
+//throw keyword
+def myMethod() {
+    if (someCondition) {
+        throw new SomeException("Some error message")
+    }
+}
+//threadsafe keyword
+def sata =
+        "threadsafe: The threadsafe keyword is not a standard Groovy keyword.it might be used in custom code or libraries toindicate that a particular piece of code is designed to be thread-safe."
